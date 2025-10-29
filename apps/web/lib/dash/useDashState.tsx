de3b2de942482d8dd0a92d368f69860ead7e4e0d
Inneren12 <chulkov.oleksii.can@gmail.com>
2025-10-29 04:32:53 -0600

    fix: satisfy typed route expectations

---
+++ apps/web/lib/dash/useDashState.tsx
@@ -0,0 +1,9 @@
+export {
+  DashStateProvider,
+  DASH_RANGE_DEFAULT,
+  DASH_RANGE_LIMITS,
+  createDefaultDashState,
+  useDashState,
+} from "./state";
+
+export type { DashState } from "./state";
